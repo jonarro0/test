bfdd96f37ed1cd6ef1fbc5da60229fd11819a029
jonarro0 <jrodriguez@cloudgenia.com>
2024-08-30 12:53:31 -0600

    asd

--- WorkerServiceApp/Program.cs
+++ WorkerServiceApp/Program.cs
@@ -1,12 +1,6 @@
 using WorkerServiceApp;
-using Microsoft.EntityFrameworkCore;
-
 
 var builder = Host.CreateApplicationBuilder(args);
-
-builder.Services.AddDbContext<context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
